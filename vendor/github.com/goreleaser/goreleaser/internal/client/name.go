20a772b53aac69d3e48dfd36244d61b697b3dc69
Vincent Prince <vincent.prince.fr@gmail.com>
2019-10-07 09:38:05 +0200

    1.7.8 with vendors

---
+++ vendor/github.com/goreleaser/goreleaser/internal/client/name.go
@@ -0,0 +1,36 @@
+package client
+
+import (
+	"bytes"
+	"text/template"
+	"time"
+
+	"github.com/goreleaser/goreleaser/context"
+)
+
+func releaseTitle(ctx *context.Context) (string, error) {
+	var out bytes.Buffer
+	t, err := template.New("github").
+		Option("missingkey=error").
+		Funcs(mkFuncMap()).
+		Parse(ctx.Config.Release.NameTemplate)
+	if err != nil {
+		return "", err
+	}
+	err = t.Execute(&out, struct {
+		ProjectName, Tag, Version string
+	}{
+		ProjectName: ctx.Config.ProjectName,
+		Tag:         ctx.Git.CurrentTag,
+		Version:     ctx.Version,
+	})
+	return out.String(), err
+}
+
+func mkFuncMap() template.FuncMap {
+	return template.FuncMap{
+		"time": func(s string) string {
+			return time.Now().UTC().Format(s)
+		},
+	}
+}
